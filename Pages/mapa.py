2e46b24c13202c56b88496ef8317e767139d93ef
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2023-11-06 17:02:17 -0300

    mapa carregando direito

--- Pages/mapa.py
+++ Pages/mapa.py
@@ -25,35 +25,18 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-def geocode_address(bairro, rua):
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    geocode = geolocator.geocode
-    
-    try:
-        location = geocode(f"{rua}, {bairro}, São Paulo")
-        if location:
-            return location.latitude, location.longitude
-        # else:
-        #     return None, None
-    except Exception as e:
-        print(f"Erro ao geocodificar {rua}, {bairro}: {e}")
-    return None, None
-
 dados_geocoded = []
 dados_json = get_problemas()
-for item in dados_json['problemas']:
-    lat, lon = geocode_address(item['bairro'], item['rua'])
-    if lat and lon:
-        item['latitude'] = lat
-        item['longitude'] = lon
-        dados_geocoded.append(item)
-
 dados_geocoded = [item for item in dados_json['problemas'] if item['latitude'] and item['longitude']]
+# convert latitude and longitude to float
+for item in dados_geocoded:
+    item['latitude'] = float(item['latitude'])
+    item['longitude'] = float(item['longitude'])
 
-
+geolocator = Nominatim(user_agent="teste")
 dados_df = pd.DataFrame(dados_geocoded)
 
-size_map = {"Alta": 15, "Mediana": 10, "Baixa": 5}
+size_map = {"Extrema": 15, "Mediana": 10, "Mínima": 5}
 dados_df['urgencia_size'] = dados_df['urgencia'].map(size_map)
 
 def create_bubble_map(dataframe):
@@ -65,7 +48,7 @@ def create_bubble_map(dataframe):
                             color_discrete_sequence=px.colors.qualitative.Pastel,  # Esquema de cores mais suave
                             zoom=10,
                             height=869,
-                            width=769)  # Altura maior do mapa
+                            width=1069)  # Altura maior do mapa
     fig.update_layout(mapbox_style="carto-positron")  # Mapa com menos informação visual
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
