024c1a52205487abec3d2772ddcae3d8a07c88cc
Esther <estherccr@al.insper.edu.com>
2023-11-06 16:10:18 -0300

    filtro arrumado

--- Pages/novos_problemas.py
+++ Pages/novos_problemas.py
@@ -7,18 +7,27 @@ options = st.multiselect(
     'Filtro de urgência',
     ['Extrema', 'Mediana', 'Mínima'])
 
-if 'Extrema' in options:
-    salvou, problemas = uts.mostra_problemas_filtrado()
-    st.write(f'{problemas}')
+# if 'Extrema' in options:
+#     filtro = {}
+#     salvou, problemas = uts.mostra_problemas_filtrado()
+#     st.write(f'{problemas}')
 
-if 'Mediana' in options:
-    salvou, problemas = uts.mostra_problemas_filtrado()
-    st.write(f'{problemas}')
+# if 'Mediana' in options:
+#     salvou, problemas = uts.mostra_problemas_filtrado()
+#     st.write(f'{problemas}')
 
-if 'Mínima' in options:
-    salvou, problemas = uts.mostra_problemas_filtrado()
-    st.write(f'{problemas}')
+# if 'Mínima' in options:
+#     salvou, problemas = uts.mostra_problemas_filtrado()
+#     st.write(f'{problemas}')
 
-else:
+if options == []:
     problemas = uts.get_problemas()
     st.write(f'{problemas}')
+else:
+    for i in range (len(options)):
+        salvou, problemas = uts.mostra_problemas_filtrado(options[i])
+    if salvou:
+        if problemas['problemas'] == []:
+            st.write("Não há problemas cadastrados com essa urgência")
+        else:
+            st.write(f'{problemas}')
--- api_utils.py
+++ api_utils.py
@@ -7,9 +7,9 @@ def get_problemas():
     response = requests.get(f'{BASE_URL}/problemas')
     return response.json()
 
-def mostra_problemas_filtrado():
+def mostra_problemas_filtrado(filtro):
     try:
-        response = requests.get(f"{BASE_URL}/problemas/filter")
+        response = requests.get(f"{BASE_URL}/problemas/filter", json={"urgencia": filtro})
         response.raise_for_status()
         return True, response.json()
     except Exception as e:
