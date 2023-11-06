bad18d2d07f30b1b779c3423fea96c6c706b3f87
laupontiroli <laurapm@al.insper.edu.br>
2023-11-06 17:25:15 -0300

    interface dos problemas feita

--- Pages/novos_problemas.py
+++ Pages/novos_problemas.py
@@ -17,6 +17,15 @@ st.markdown("""
         position: relative;
         top: 100px;
         }
+    .mid {
+        color:rgba(255,140,0);
+    }
+    .alt {
+        color:rgba(255,69,0)
+    }
+    .low {
+        color:rgba(255,215,0)
+    }
 </style>
 """, unsafe_allow_html=True)
 
@@ -37,14 +46,70 @@ options = st.multiselect(
 #     salvou, problemas = uts.mostra_problemas_filtrado()
 #     st.write(f'{problemas}')
 
+datas =[]
+status = [] 
+tipos_problemas = []
+urgencias=[]
 if options == []:
-    problemas = uts.get_problemas()
-    st.write(f'{problemas}')
+    deu_certo,problemas = uts.get_problemas()
+    if deu_certo: 
+        for problema in problemas['problemas']:
+            datas.append(problema['data_inicio'])
+            if 'status' in problema:
+                status.append(problema['status'])
+            tipos_problemas.append(problema['problema_tipo'])
+            urgencias.append(problema['urgencia'])
+    else: 
+        st.error(problemas)
+
 else:
     for i in range (len(options)):
-        salvou, problemas = uts.mostra_problemas_filtrado(options[i])
-    if salvou:
+        deu_certo, problemas = uts.mostra_problemas_filtrado(options[i])
+    if deu_certo:
         if problemas['problemas'] == []:
             st.write("Não há problemas cadastrados com essa urgência")
         else:
-            st.write(f'{problemas}')
+            for problema in problemas['problemas']:
+                datas.append(problema['data_inicio'])
+                if 'status' in problema:
+                    status.append(problema['status'])
+                tipos_problemas.append(problema['problema_tipo'])
+                urgencias.append(problema['urgencia'])
+    else: 
+        st.error(problemas)
+
+
+col1,col2,col3,col4,col5 = st.columns([1,1,1,1,1])
+
+with col1:
+    st.subheader("Problema")
+    st.divider()
+    for tipo_problema in tipos_problemas:
+        st.write(tipo_problema)
+
+with col2:
+    st.subheader("Datas")
+    st.divider()
+    for data in datas:
+        st.write(data)
+
+with col3:
+    st.subheader("Urgência")
+    st.divider()
+    for urgencia in urgencias:
+        if urgencia == "Alta" or urgencia == "alta":
+            st.write(f'<h6 class= alt>{urgencia} </h6>',unsafe_allow_html=True)
+        if urgencia == "Mediana" or urgencia == "mediana":
+            st.write(f'<h6 class= mid>{urgencia} </h6>',unsafe_allow_html=True)
+        if urgencia == "Mínima" or  urgencia == "minima":
+            st.write(f'<h6 class= low>{urgencia} </h6>',unsafe_allow_html=True)
+
+with col4:
+    st.subheader("Status")
+    st.divider()
+    for status_ in status:
+        st.write(status_)
+
+with col5:
+    st.subheader("Mais")
+    st.write()
