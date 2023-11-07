3c2c830fd2b01ec9b3cb74833a25aec6be2dce83
Rafaela Afférri de Oliveira <rafaelaao@al.insper.edu.br>
2023-11-07 08:37:57 -0300

    concertando mudar status

--- api_utils.py
+++ api_utils.py
@@ -1,7 +1,7 @@
 import requests
 
 
-BASE_URL = "https://reporta-cidade-36c5016e43fd.herokuapp.com"
+BASE_URL = "http://127.0.0.1:4800"
 
 def get_problemas():
     try:
@@ -13,6 +13,14 @@ def get_problemas():
 def mostra_problemas_filtrado(filtro):
     try:
         response = requests.get(f"{BASE_URL}/problemas/filter", json={"urgencia": filtro})
+        response.raise_for_status()
+        return True, response.json()
+    except Exception as e:
+        return False, {"message": str(e)}
+    
+def update_problemas(problema):
+    try:
+        response = requests.put(f"{BASE_URL}/problemas", json=problema)
         response.raise_for_status()
         return True, response.json()
     except Exception as e:
